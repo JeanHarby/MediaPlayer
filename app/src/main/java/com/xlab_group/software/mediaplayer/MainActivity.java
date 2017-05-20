65900aa470ab9baf35b53e105698054af3ef8db3
Jean.Harby <Jean.Harby@gmail.com>
2017-05-20 16:32:05 +0200

    finished  no vol control

--- app/src/main/java/com/xlab_group/software/mediaplayer/MainActivity.java
+++ app/src/main/java/com/xlab_group/software/mediaplayer/MainActivity.java
@@ -1,13 +1,36 @@
 package com.xlab_group.software.mediaplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this.getApplicationContext(), R.raw.a1);
+    }
+
+    public void PlaySound(View view) {
+        mediaPlayer.start();
+    }
+
+    public void PauseSound(View view) {
+        mediaPlayer.pause();
+    }
+
+    public void forward(View view) {
+        mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() + 30000);
+    }
+
+    public void volUp(View view) {
+        mediaPlayer.setVolume(50, 50);
+    }
+
+    public void volDown(View view) {
+        mediaPlayer.setVolume(1, 1);
     }
 }
